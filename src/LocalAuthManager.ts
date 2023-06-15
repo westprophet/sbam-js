6df629e333bc563da3ff9d8f62bfea97aec4774c
westprophet <westprophet@gmail.com>
2023-06-16 00:09:04 +0300

    upd logic by objects

--- src/LocalAuthManager.ts
+++ src/LocalAuthManager.ts
@@ -18,15 +18,21 @@ export default class LocalAuthManager<T = string> {
   /** Тип хранения данних */
   protected storage: any;
 
+  /**
+     * Если токен это json то нужно парсить его при входе и выходе
+     */
+
   /** токен */
   private _token: T | null = null;
 
   constructor(
     storageType: 'localStorage' | 'sessionStorage' = 'sessionStorage', // выбор провайдера хранилища
     tokenKey = 'wt',
+    parse = false,
   ) {
     this.storage = storageType === 'localStorage' ? localStorage : sessionStorage;
     this.tokenKey = tokenKey;
+    this.parse = parse;
     const localToken = this.getLocalStorageToken();
     this._token = localToken ?? null;
   }
@@ -40,42 +46,58 @@ export default class LocalAuthManager<T = string> {
   }
 
   /**
-   * Установка токена в локальное хранилище
-   * @memberof LocalAuthManager
-   * @param token
-   */
+     * Установка токена в локальное хранилище
+     * @memberof LocalAuthManager
+     * @param token
+     */
+
+  // eslint-disable-next-line class-methods-use-this
+  private isCanStringify(token: T): boolean {
+    return typeof token === 'object';
+  }
 
   private setStorageToken(token: T) {
-    if (this.instanceOfToken(token)) { this.storage.setItem(this.tokenKey, token); }
+    if (this.instanceOfToken(token)) {
+      const a = this.isCanStringify(token);
+      this.storage.setItem(this.tokenKey, a ? JSON.stringify(token) : token);
+    }
   }
 
   /**
-   * Получение токена из локального хранилища
-   * @memberof LocalAuthManager
-   * @return {token | null}
-   */
+     * Получение токена из локального хранилища
+     * @memberof LocalAuthManager
+     * @return {token | null}
+     */
   public getLocalStorageToken(): T | null {
     const token: any = this.storage.getItem(this.tokenKey);
-    return this.instanceOfToken(token) ? token : null;
+    if (!token) return null;
+    let res: T;
+    try {
+      res = JSON.parse(token);
+    } catch (e) {
+      res = token;
+    }
+    return this.instanceOfToken(res) ? res : null;
   }
 
   /**
-   * Проверка токена на соответсвие типов, является ли переменная данного типа
-   * @memberof LocalAuthManager
-   * @param object
-   */
+     * Проверка токена на соответсвие типов, является ли переменная данного типа
+     * @memberof LocalAuthManager
+     * @param object
+     */
+  // eslint-disable-next-line class-methods-use-this
   public instanceOfToken(object: any): boolean {
     return !!object;
   }
 
   /**
-   * Авторизоватся локально в системе фронтенда
-   * @memberof LocalAuthManager
-   * @param newToken
-   * @public
-   * @return {boolean}
-   */
-  public login(newToken: any): boolean {
+     * Авторизоватся локально в системе фронтенда
+     * @memberof LocalAuthManager
+     * @param newToken
+     * @public
+     * @return {boolean}
+     */
+  public login(newToken: T): boolean {
     if (this.instanceOfToken(newToken)) {
       try {
         this.setStorageToken(newToken);
@@ -84,15 +106,16 @@ export default class LocalAuthManager<T = string> {
         return false;
       }
       return true;
-    } return false;
+    }
+    return false;
   }
 
   /**
-   * Разлогинится в системе
-   * Удаляет ключ из менеджера и хранилищь
-   * @memberof LocalAuthManager
-   * @return {boolean} индикатор удачного удаления данных из системы
-   */
+     * Разлогинится в системе
+     * Удаляет ключ из менеджера и хранилищь
+     * @memberof LocalAuthManager
+     * @return {boolean} индикатор удачного удаления данных из системы
+     */
   public logout(): boolean {
     try {
       this.token = null;
